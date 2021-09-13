ac87ff49b8158ac671f3b76d22b49413a4f605d8
Joesh Gichon <joeshgichon56@gmail.com>
2021-09-13 20:46:40 +0300

    moved the config file inside the app folder into the root folder then exposing the API KEY to the global environment

--- config.py
+++ config.py
@@ -1,9 +1,17 @@
+import os
+
 class Config:
      NEWS_API_BASE_URL ="https://newsapi.org/v2/top-headlines/sources?apiKey={}"
      ARTICLE_API_BASE_URL="https://newsapi.org/v2/top-headlines?sources={}&apiKey={}"
+     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
 
 class ProdConfig(Config):
     pass
 
 class DevConfig(Config):
     DEBUG = True
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
